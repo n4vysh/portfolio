db001f59fe113348ffe63bc3140141afd71af415
n4vysh <n4vysh@n4vysh.dev>
2021-11-27 00:12:20 +0900

    chore: add trex

--- scripts/init.bash
+++ scripts/init.bash
@@ -11,6 +11,20 @@ if ! type denon >/dev/null 2>&1; then
   deno install -qAf --unstable https://deno.land/x/denon@2.4.10/denon.ts
 fi
 
+if ! type trex >/dev/null 2>&1; then
+  # trex is not support xdg directory
+  # https://github.com/crewdevio/Trex/blob/v1.10.0/utils/storage.ts#L40
+  # https://github.com/denoland/deno/issues/1241
+  [[ ! -d ~/.deno ]] && mkdir ~/.deno
+  deno install \
+    -A \
+    --unstable \
+    --import-map=https://deno.land/x/trex@v1.10.0/import_map.json \
+    -n trex \
+    --no-check \
+    https://deno.land/x/trex@v1.10.0/cli.ts
+fi
+
 if ! type aleph >/dev/null 2>&1; then
   denon install:aleph
 fi
