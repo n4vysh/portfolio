bbf449ba197f9542fe76b72188db9694ee832024
n4vysh <n4vysh@n4vysh.dev>
2022-04-09 23:23:18 +0900

    fix(infra/terraform): change configuration_aliases from aws.us_east_1 to aws.secondary

--- infra/terraform/modules/aws-route53-dnssec/versions.tf
+++ infra/terraform/modules/aws-route53-dnssec/versions.tf
@@ -3,7 +3,7 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = "~> 4.2.0"
-      configuration_aliases = [aws.us_east_1]
+      configuration_aliases = [aws.secondary]
     }
   }
 
