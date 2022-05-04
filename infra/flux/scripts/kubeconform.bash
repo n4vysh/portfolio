21ef6762dbed7a3ece293cb21a5664976f3788fb
n4vysh <n4vysh@n4vysh.dev>
2022-05-04 17:41:06 +0900

    refactor(infra/flux): change directory structure

--- infra/flux/scripts/kubeconform.bash
+++ infra/flux/scripts/kubeconform.bash
@@ -11,16 +11,23 @@ opts=(
 	-schema-location "./crd-schemas/{{ .ResourceKind }}{{ .KindSuffix }}.json"
 	-verbose
 	-strict
-	-skip 'Gateway,PodMonitor,ServiceMonitor'
+	-skip 'Gateway,PodMonitor,ServiceMonitor,Canary'
 )
 
-set -x
-kubeconform "${opts[@]}" "./clusters/production/"*.yaml
-# use kustomize-controller build options
-kubectl kustomize \
-	--load-restrictor=LoadRestrictionsNone \
-	--reorder=legacy \
-	"./infra/overlays/production/" |
-	kubeconform "${opts[@]}"
-# need crd schemas of prometheus-operator, istio, and flagger
-# kubectl kustomize apps/overlays/production/ | kubeconform "${opts[@]}"
+(
+	set -x
+	kubeconform "${opts[@]}" "./clusters/production/"*.yaml
+)
+
+mapfile -t dirs < <(find kustomize/ -mindepth 1 -maxdepth 1 -type d)
+for dir in "${dirs[@]}"; do
+	(
+		# use kustomize-controller build options
+		set -x
+		kubectl kustomize \
+			--load-restrictor=LoadRestrictionsNone \
+			--reorder=legacy \
+			"./$dir/overlays/production/" |
+			kubeconform "${opts[@]}"
+	)
+done
