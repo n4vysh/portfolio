22e707850a7d432d000d6310042647d520d0755a
n4vysh <n4vysh@n4vysh.dev>
2022-05-07 20:32:12 +0900

    feat(infra/flux): enable anonymous authentication of Grafana

--- infra/scripts/viz-monitor.bash
+++ infra/scripts/viz-monitor.bash
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-cat <<-'EOF'
-	To check username and password of Grafana admin user, use following commands in another terminal window
-
-	```bash
-	kubectl -n kube-prometheus-stack get secret kube-prometheus-stack-grafana -o json |
-	  jq '.data |
-	  map_values(@base64d)'
-	```
-
-EOF
-
 kubectl -n kube-prometheus-stack port-forward svc/kube-prometheus-stack-grafana 8080:80 &
 sleep 2
 xdg-open http://localhost:8080
